5e8f1abb99f4924098e807aec73e3cdb9271d942
Felipe Freitas <felipefreitas0015@gmail.com>
2025-05-15 19:23:04 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,20 @@
 <?php
 
+$dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
-$dbName = 'config';
-$dbHost = 'localhost';
+$dbName = 'confeitaria_la_vic';
     
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+//  If($conexao->connect_errno)
+//{
+//  echo "Erro ao conectar". $conexao->connect_error;
+//
+//}
+//  else
+//{
+//  echo "Conexão efetuada com sucesso";
+//}
 
 ?>
