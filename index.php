20432fb71336b3cefb5fa8a72c8a8b94e4be2a4b
Karen Nakaoka <karenkn.28@gmail.com>
2025-05-15 13:40:36 -0300

    Adding index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Confeitaria La Vic</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+  <header>
+    <a href="index.html"><img src="img/logo.png" alt="Confeitaria La Vic" class="logo" /></a>
+    <nav>
+      <a href="login.html">Entre ou cadastre-se</a>
+    </nav>
+  </header>
+
+  <main>
+    <section class="intro">
+      <h2>Bem-vindo(a)!</h2>
+      <p>Aqui a gente pode por um texto de apresentação ou uma frase de efeito</p>
+      <p>A qui em baixo a gente pode por o texto de apresentação se já não tivermos colocado no primerio paragrafo.</p>
+    </section>
+
+    <section class="vitrine">
+      <h2>Nossas Delícias</h2>
+      <div class="produtos">
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 01</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 02</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 03</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 04</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 05</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 06</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 07</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 08</p>
+        </div>
+        <div class="produto">
+            <img src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" alt="Doce">
+            <p>Bolo ou Doce 09</p>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <div class="rodape">
+      <h3>Contato</h3>
+      <p>Whatsapp: (11) XXXXX-XXXX</p>
+      <p>Instagram: @XXXXXXXX</p>
+      <br>
+     &copy; 2025 Confeitaria La Vic. Todos os direitos reservados.
+    </div>
+  </footer>
+</body>
+</html>
